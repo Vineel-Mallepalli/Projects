b7fb5f91ba8b7839460e12dd70e6207560a74730
Vineel-Mallepalli <vrm2@rice.edu>
2020-05-31 13:51:39 -0500

    added pokernow log parsing tool for a single banker. Keeps track of multiple buy-ins and end net amounts for players, as well as if a player has quit or not. Condenses same player name buy-ins. TODO: check and adjust for manual owner amt-changing transactions. TODO: see if you can access and download the log given url and perform analysis on log. Then we can also access end amounts.

---
+++ Home Projects/poker/display_ranges.py
@@ -0,0 +1,8 @@
+
+
+# display a colored board of given ranges
+# ranges are given in the following format: [[premiums/raises], [calls]]
+def display_range(range):
+    if len(range) != 2:
+        return
+    
--- Home Projects/poker/identify_payments.py
+++ Home Projects/poker/identify_payments.py
@@ -1,13 +1,16 @@
 from decimal import *
+from _collections import defaultdict
 import pandas as pd
 import os
 
 
+# Parse result data from poker2 app and write instructions to excel file.
 # Assumption: if no "Name" column, first column of data contains player names
 # Requires: "Net" column
-def get_data():
+def get_data_poker2(path=""):
     getcontext().prec = 106
-    path = input("Enter the path of your file: ")
+    if path == "":
+        path = input("Enter the path of your file: ")
     split_path = path.rsplit("\\", 1)
     filename = split_path[-1]
     directory = "".join(split_path[:-1]) + "\\"
@@ -16,7 +19,7 @@ def get_data():
     sheets = xl.sheet_names
     df = xl.parse(sheets[0])
     if "Net" not in df:
-        print("'Net' column missing from data")
+        print("No 'Net' column in data.")
         return
     if "Name" not in df:
         if df.columns.get_loc("Net") == 0:
@@ -48,6 +51,62 @@ def get_data():
     return
 
 
+def condense_player_net_dict(player_net_dict):
+    # print("Before: ", player_net_dict)
+    new_player_net_dict = defaultdict(lambda: (0, False))
+    for player, amt_status_tup in player_net_dict.items():
+        new_player_net_dict[player.partition(" @")[0]] = new_player_net_dict[player.partition(" @")[0]][0] + \
+                                                         amt_status_tup[0], amt_status_tup[1]
+    # print("After: ", new_player_net_dict)
+    return new_player_net_dict
+
+
+def order_instructions(instructions):
+    unrecorded_instructions = [instruction for instruction in instructions if "has NOT been recorded" in instruction]
+    new_instructions = [instruction for instruction in instructions if instruction not in unrecorded_instructions]
+    new_instructions.extend(unrecorded_instructions)
+    return new_instructions
+
+
+# need to check what log and program do if owner manually changes player's stack.
+def get_data_pokernow(path=""):
+    if path == "":
+        path = input("Enter the path of your file: ")
+    assert os.path.exists(path), "No file could be discovered at " + path
+    csv = pd.read_csv(path)
+    game_history = list(csv[csv.keys()[0]])
+    buy_in_out_list = [event for event in game_history
+                       if "quits the game" in event or "created the game" in event or "approved the player" in event]
+    # print(buy_in_out_list, len(buy_in_out_list), sep="\n")
+    # print([event for event in buy_in_out_list if "vaid" in event])
+    player_net_dict = defaultdict(lambda: (0, False))  # player : tuple(net, has_quit)
+    for i in range(len(buy_in_out_list) - 1, -1, -1):
+        buy_in_out_event = buy_in_out_list[i]
+        earnings = 1 if "quits the" in buy_in_out_event else -1
+        quitting = True if "quits the" in buy_in_out_event else False
+        amount = int(buy_in_out_event.rsplit(" ", 1)[-1][:-1])
+        player = buy_in_out_event.split("\"")[1]
+        # check what happens if player has quit before but is now buying back in. does NOT provide player a new @ hash.
+        if player_net_dict[player][1] and quitting:  # error check: player has already quit and is trying to quit again.
+            print("Error: player %s has quit and is now performing %s event %d" % (player, buy_in_out_event, i))
+            return
+        player_net_dict[player] = player_net_dict[player][0] + earnings * amount, quitting
+    player_net_dict = condense_player_net_dict(player_net_dict)
+    instructions = []
+    for player, amount_status_tup in player_net_dict.items():
+        keyword = "Pay "
+        if amount_status_tup[1]:  # player has quit
+            if amount_status_tup[0] < 0:
+                keyword = "Request "
+                amount_status_tup = -amount_status_tup[0], amount_status_tup[1]
+            instructions.append(keyword + player + " " + str(amount_status_tup[0]))
+        else:
+            instructions.append("%s has a current net of %d. End amt has NOT been recorded in log."
+                                % (player, amount_status_tup[0]))
+    print(*order_instructions(instructions), sep="\n")
+    return
+
+
 # Does not require list to be sorted, but helps with efficiency.
 # Requires: list amounts are net-zero
 def apply_alg(nets):
@@ -61,8 +120,6 @@ def apply_alg(nets):
             payers.append(i)
         else:
             receivers.append(i)
-    # payers.sort()
-    # receivers.sort()
     curr_rec = 0
     for payer in payers:
         payout = [payer, []]
@@ -82,4 +139,39 @@ def apply_alg(nets):
     return payouts
 
 
-get_data()
+def single_banker(player_data):
+    list_player_data = player_data.split(";")[:-1]
+    player_net_dict = {}
+    for player_data in list_player_data:
+        partition = player_data.partition("=")
+        if partition[0].strip() not in player_net_dict:
+            player_net_dict[partition[0].strip()] = [partition[2].strip()]
+        else:
+            val = player_net_dict[partition[0].strip()]
+            val.append(partition[2].strip())
+            player_net_dict[partition[0].strip()] = val
+    # print(player_net_dict)
+    for key, value_lst in player_net_dict.items():
+        net = Decimal(0)
+        for value in value_lst:
+            if "paid" not in value:
+                net += Decimal(value.partition(" ")[2]).quantize(Decimal(".01"), rounding=ROUND_HALF_UP) - \
+                       Decimal(value.partition(" ")[0]).quantize(Decimal(".01"), rounding=ROUND_HALF_UP)
+        player_net_dict[key] = net
+    # print(player_net_dict)
+
+    for key, value in player_net_dict.items():
+        if isinstance(value, Decimal):
+            keyword = "Pay "
+            if value < 0:
+                keyword = "Request "
+                value = -value
+            print(keyword + key + " " + str(value))
+    return
+
+
+get_data_pokernow("C:\\Users\\vinee\\PycharmProjects\\Home Projects\\poker\\poker_test_log_2.csv")
+data = "Vineel = 10 70.75; Liam = 20 9.20; Muthu = 20 10.28; Will = 20 0; Eric = 7 13.03; Varun = 20 1.62; " \
+       "Vaidya = 10 3.36; Cole = 10 15.90; Nolan = 7 paid; Liam = 35 0; Vineel = 15 51.69; Cole = 30 29.75; " \
+       "Will = 15 31.94; Connor = 10 10.62; Jacob = 10 1.0; Vaidya = 10 0;"
+# single_banker(data)
